90133fcfe15192d68b015bc8a8fa549d4d177328
Wirestheyeen <msohnen@wiredhyena.com>
2018-11-13 17:20:52 -0500

    some more work on spider.h

--- spider.h
+++ spider.h
@@ -4,23 +4,22 @@
 using namespace mys;
 namespace spider {
 	class spider {
-		const int numPoints = 1 + 6 + 6 + 6 + 1;
+		const int numTrs  = 6+12+12+6;
 		const float backLength = 50;
 		const float backRadius = 20;
 		const float frontLength = 25;
 		const float frontRadius = 12;
 		vec2 center, direction;
-		boost::scoped_array<vec3> mesh;
+		boost::scoped_array<triangle3> mesh;
 		void Spider(vec2 center, vec2 direction) {
 			center = center;
 			direction = normalize(direction);
 
 		}
 		void build() {
-			mesh.reset(new vec3[numPoints]);
+			mesh.reset(new triangle3[numTrs]);
 			int ct = 0;
-		//s	mesh[ct++]=add(center,scalarMultiply());
-			
+	
 			
 			
 
